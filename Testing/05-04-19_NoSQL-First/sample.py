a28996c7c42b28002af2ff1e83e12c696cb6d39b
Mayank Baranwal <35839549+Mayank-Baranwal@users.noreply.github.com>
2019-04-06 03:16:07 +0530

    Update sample.py

--- Testing/05-04-19_NoSQL-First/sample.py
+++ Testing/05-04-19_NoSQL-First/sample.py
@@ -8,21 +8,22 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-doc_ref = db.collection(u'rooms').document(u'cs222')
+doc_ref = db.collection(u'Courses').document(u'CS243').get()
 # path = "users/lace";
-ref = db.collection(u'users').document(u'lace')
-ref2 = db.collection(u'users').document(u'elace')
-doc_ref.set({
-    u'size': 50,
-    u'mem1': ref2,
-    u'mem2': ref
-})
+# ref = db.collection(u'users').document(u'lace')
+# ref2 = db.collection(u'users').document(u'elace')
+# doc_ref.set({
+#     u'size': 50,
+#     u'mem1': ref2,
+#     u'mem2': ref
+# })
 
 try:
-    doc = doc_ref.get().to_dict()
+    # doc = doc_ref.get().to_dict()
     #dict = doc.to_dict()
-    mm = doc['mem2'].get()
+    # mm = doc['mem2'].get()
+
     #print(dict['mem2'])
-    print(u'Document data: {}'.format(mm.to_dict()))
+    print(u'Document data: {}'.format(doc_ref.to_dict()))
 except google.cloud.exceptions.NotFound:
     print(u'No such document!')
