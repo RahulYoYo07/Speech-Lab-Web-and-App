eebee125f6f78821245139541f3f57c185a21efe
LavishGulati <lavish00gulati@gmail.com>
2019-04-07 00:32:15 +0530

    Intialized discussion room structure

--- IITG_Speech_Lab/discussion/views.py
+++ IITG_Speech_Lab/discussion/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def course_group(request, CourseID, CourseGroupID):
+    pass
+
+def group(request, CourseID, AssignmentID, GroupID):
+    pass
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lavishgulati/Github/Speech-Lab-Web-and-App/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
