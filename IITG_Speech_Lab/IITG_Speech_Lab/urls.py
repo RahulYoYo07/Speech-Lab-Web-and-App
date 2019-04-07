f2d56725c168e642a01338b4bc213b9a955eabcd
RAVI SHANKAR <ravi>
2019-04-07 16:04:50 +0530

    sidebar basic

--- IITG_Speech_Lab/IITG_Speech_Lab/urls.py
+++ IITG_Speech_Lab/IITG_Speech_Lab/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('discussion/', include('discussion.urls')),
+    path('course/', include('course.urls')),
 ]
