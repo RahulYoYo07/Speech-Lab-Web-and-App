6f94f4d37af65fa3ef65583a1d7a64c9fd4dc0d4
Mani Mannampalli <mani.mannampalli@gmail.com>
2019-04-07 23:29:57 +0530

    View Course Page

--- IITG_Speech_Lab/IITG_Speech_Lab/urls.py
+++ IITG_Speech_Lab/IITG_Speech_Lab/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('discussion/', include('discussion.urls')),
-    path('course/', include('course.urls')),
+    path('courses/', include('course.urls')),
 ]
