1e1e3f06adf24ddf771aede99ca75fba507044a0
Udbhav Chugh <udbha170123055@iitg.ac.in>
2019-04-13 21:23:08 +0530

    Added notice board

--- django/discussion/urls.py
+++ django/discussion/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('courses/<slug:CourseID>/coursegroup/<slug:CourseGroupID>', views.course_group, name='course_group'),
     # Course individual groups
     path('courses/<slug:CourseID>/assignments/<slug:AssignmentID>/groups/<slug:GroupID>', views.group, name='group'),
+    #Course Notice Board
+    path('courses/<slug:CourseID>/noticeboard', views.notice_board, name='notice_board'),
+    #Add Notice
+    path('courses/<slug:CourseID>/noticeboard/addnotice', views.add_notice, name='add_notice'),
+
 ]
