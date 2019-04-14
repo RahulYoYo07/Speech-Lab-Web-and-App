81d309a8eb45717cecf3cec46515593ddfec0308
Hansraj Patel <hansr170101025@iitg.ac.in>
2019-04-15 03:29:00 +0530

    App chalra

--- android/app/src/main/java/com/example/iitg_speech_lab/CoursesActivity.java
+++ android/app/src/main/java/com/example/iitg_speech_lab/CoursesActivity.java
@@ -4,13 +4,34 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Context;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class CoursesActivity extends AppCompatActivity {
 
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<CoursesDataModel> data;
+    static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String username = "pradip";
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -24,6 +45,95 @@ public class CoursesActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        myOnClickListener = new MyOnClickListener(this);
+
+        recyclerView = ( RecyclerView ) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        CoursesMyData.loadData(username);
+
+        data = new ArrayList<CoursesDataModel>();
+        for (int i = 0; i < CoursesMyData.courseIDArray.length; i++) {
+            data.add(new CoursesDataModel(
+                    CoursesMyData.courseIDArray[i],
+                    CoursesMyData.courseNameArray[i],
+                    CoursesMyData.id_[i]
+            ));
+        }
+
+        removedItems = new ArrayList<Integer>();
+
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 
+
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder
+                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+            TextView textViewName
+                    = viewHolder.itemView.findViewById(R.id.textViewName);
+            String selectedName = ( String ) textViewName.getText();
+            int selectedItemId = -1;
+            for (int i = 0; i < CoursesMyData.courseIDArray.length; i++) {
+                if (selectedName.equals(CoursesMyData.courseIDArray[i])) {
+                    selectedItemId = CoursesMyData.id_[i];
+                }
+            }
+            removedItems.add(selectedItemId);
+            data.remove(selectedItemPosition);
+            adapter.notifyItemRemoved(selectedItemPosition);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_courses, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.add_item) {
+            //check if any items to add
+            if (removedItems.size() != 0) {
+                addRemovedItemToList();
+            } else {
+                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return true;
+    }
+
+    private void addRemovedItemToList() {
+        int addItemAtListPosition = 3;
+        data.add(addItemAtListPosition, new CoursesDataModel(
+                CoursesMyData.courseIDArray[removedItems.get(0)],
+                CoursesMyData.courseNameArray[removedItems.get(0)],
+                CoursesMyData.id_[removedItems.get(0)]
+        ));
+        adapter.notifyItemInserted(addItemAtListPosition);
+        removedItems.remove(0);
+    }
 }
