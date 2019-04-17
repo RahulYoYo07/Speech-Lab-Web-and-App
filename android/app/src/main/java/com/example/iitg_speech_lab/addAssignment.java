6e0de9f6fafb854ecff9164990bc9913ff40f2e6
Piyush <piyushg1411@gmail.com>
2019-04-17 16:47:12 +0530

    added upload assignment support

--- android/app/src/main/java/com/example/iitg_speech_lab/addAssignment.java
+++ android/app/src/main/java/com/example/iitg_speech_lab/addAssignment.java
@@ -1,5 +1,10 @@
 package com.example.iitg_speech_lab;
 
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +12,31 @@ import android.util.Log;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Array;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 import com.google.firebase.database.DataSnapshot;
@@ -32,32 +60,51 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Map;
 
 public class addAssignment extends AppCompatActivity {
+    //a constant to track the file chooser intent
+    private static final int PICK_FILE_REQUEST = 234;
+    private static final String TAG = "AddAssignmentActivity";
+    public String gid = "AS$$";
+    String cinfo = "HS241_pradip_2019";
+    String durl = "";
+    String selDate = "";
+    //a Uri object to store file path
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assignment);
         String userName = "pradip";
+        CalendarView calender = findViewById(R.id.calendarView);
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                // TODO Auto-generated method stub
+
+                selDate = dayOfMonth +" / " + (month+1) + " / " + year;
+
+            }
+        });
     }
-    private static final String TAG = "AddAssignmentActivity";
-    public String  gid = "AS$$";
 
-
-    public void addAssgn(View view){
+    public void addAssgn(View view) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String cinfo = "HS241_pradip_2019";
 
         db.collection("Courses").document(cinfo).collection("Assignments").get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
 
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            gid = "AS" + Integer.toString(task.getResult().size()+1);
+                            gid = "AS" + Integer.toString(task.getResult().size() + 1);
                             up();
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
@@ -67,22 +114,19 @@ public class addAssignment extends AppCompatActivity {
 
     }
 
-    private void up(){
+    private void up() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String cinfo = "HS241_pradip_2019";
 
-        Map<String, Object> newassg = new HashMap <>();
+        Map<String, Object> newassg = new HashMap<>();
         EditText About = findViewById(R.id.About);
         EditText Title = findViewById(R.id.Title);
-        CalendarView dt = findViewById(R.id.calendarView);
-//        SimpleDateFormat ss = new SimpleDateFormat("dd-MM-yyyy");
-
-
 
         newassg.put("About", About.getText().toString());
         newassg.put("Name", Title.getText().toString());
         newassg.put("AssignmentID", gid);
-        newassg.put("Deadline",dt.getDateTextAppearance() );
+        newassg.put("Deadline", selDate);
+        newassg.put("Resources", Arrays.asList(durl));
+
         Log.d(TAG, gid);
 
 
@@ -100,4 +144,90 @@ public class addAssignment extends AppCompatActivity {
                     }
                 });
     }
+
+    //method to show file chooser
+    public void showFileChooser(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent, PICK_FILE_REQUEST);
+    }
+
+    //handling the image chooser activity result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FILE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Button buttonChoose = findViewById(R.id.buttonChoose);
+            filePath = data.getData();
+            buttonChoose.setText(filePath.toString());
+        }
+    }
+
+
+    //this method will upload the file
+    public void uploadFile(View view) {
+        //if there is a file to upload
+        if (filePath != null) {
+            //displaying a progress dialog while upload is going on
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+
+            // Create a storage reference from our app
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            final StorageReference storageRef = storage.getReference();
+
+            // File or Blob
+//            file = Uri.fromFile(new File("path/to/mountains.jpg"));
+
+            final StorageReference ref = storageRef.child("Courses/" + cinfo + "/" + filePath.getLastPathSegment());
+            UploadTask uploadTask = ref.putFile(filePath);
+
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    System.out.println("Upload is " + progress + "% done");
+                }
+            }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                    System.out.println("Upload is paused");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    System.out.println("Upload is complete");
+                    progressDialog.hide();
+                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
+                    {
+                        @Override
+                        public void onSuccess(Uri downloadUrl)
+                        {
+                            durl=downloadUrl.toString();
+                        }
+                    });
+                }
+            });
+        }
+        //if there is not any file
+        else {
+            //you can display an error toast
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Upload Uncessful",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+
+
 }
